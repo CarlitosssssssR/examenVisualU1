8b5d93ef7c3c1625539983200dda2a075e85d6b2
Carlitos <carlos.giovanni.ramos.work@gmail.com>
2026-03-23 17:21:04 -0500

    PintarCelda

--- src/componentesGrupo4/Interfaz.java
+++ src/componentesGrupo4/Interfaz.java
@@ -102,6 +102,7 @@ public void devolverDatosTabla() {
         }
         modeloTabla.addRow(filas);
     }*/
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
